dfc856354fa2f05da1856153e2495ded40e227d2
Pablo Graffigna <pabloggraffigna@gmail.com>
2021-04-11 12:05:08 -0300

    quinta version

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-VERSION=1.21.2
+#Colores
+greenColor="\e[0;32m\033[1m"
+redColor="\e[0;31m\033[1m"
+yellowColor="\e[0;33m\033[1m"
+endColor="\033[0m\e[0m"
 
-echo "++ Descarga docker-compose ++"
+VERSION="1.21.2"
+
+trap ctrl_c INT
+function ctrl_c(){
+        echo -e "\n${redColor}Programa Terminado ${endColor}"
+        exit 0
+}
+
+echo -e "${yellowColor}Descarga docker-compose ${endColor}"
 sudo curl -sL "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-`uname -s`-`uname -m`" -o /usr/local/bin/docker-compose
 
-echo "++ Configura los permisos de ejecución ++"
+echo -e "${yellowColor}Configura los permisos de ejecución ${endColor}"
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo -e "${greenColor}Todos los procesos terminaron correctamente!!! ${endColor}"
--- docker.sh
+++ docker.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
-echo "++ Eliminar otras versiones de Docker"
-sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt autoremove -y
 
-echo "++ Actualiza la cache de los repos ++"
-sudo apt update
+#Colores
+greenColor="\e[0;32m\033[1m"
+redColor="\e[0;31m\033[1m"
+yellowColor="\e[0;33m\033[1m"
+blueColor="\e[0;34m\033[1m"
+purpleColor="\e[0;35m\033[1m"
+endColor="\033[0m\e[0m"
 
-echo "++ Instalación de dependencias ++"
-sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent  software-properties-common
+trap ctrl_c INT
+function ctrl_c(){
+        echo -e "\n${redColor}Programa Terminado ${endColor}"
+        exit 0
+}
 
-echo "++ Agrega la llave GPG de Docker ++"
-curl -fsSL  https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+LLAVE="https://download.docker.com/linux/ubuntu/gpg"
+USUARIO="USUARIO"
+URL="https://download.docker.com/linux/ubuntu"
 
-echo "++ Agrega el repositorio stable ++"
+echo -e "${yellowColor}Elimina otras versiones de Docker ${endColor}"
+sudo apt remove docker docker.io containerd runc; sudo apt autoremove -y
+
+echo -e "\n${yellowColor}Instalación de dependencias ${endColor}"
+sudo apt update; sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent  software-properties-common
+
+echo -e "\n${yellowColor}Agrega la llave ${endColor}"
+curl -fsSL "$LLAVE" | sudo apt-key add -
+
+echo -e "\n${yellowColor}Agrega el repositorio ${endColor}"
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] $URL \
    $(lsb_release -cs) \
    stable"
 
-echo "++ Actualiza la cache de los repos ++"
-sudo apt update
+echo -e "\n${yellowColor}Instala docker-ce ${endColor}"
+sudo apt update; sudo apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
 
-echo "++ Instala la version 18.06.1 de Docker ++" 
-sudo apt install -y docker-ce=18.06.1~ce~3-0~ubuntu
+echo -e "\n${yellowColor}Agrega el usuario al grupo docker ${endColor}"
+sudo usermod -aG docker "$USUARIO" && sudo newgrp docker
 
-echo "++ Agrega el usuario vagrant al grupo docker ++"
-usermod -aG docker vagrant
+echo -e "${greenColor}Todos los procesos terminaron correctamente !!! ${endColor}"
